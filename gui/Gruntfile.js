8b22e9bfb6b4304409cba6d8b57c3d77fc3f45e2
niquola <niquola@gmail.com>
2014-05-26 19:33:02 +0400

    gui rework

--- gui/Gruntfile.js
+++ gui/Gruntfile.js
@@ -1,445 +1,114 @@
-// Generated on 2014-03-25 using generator-angular 0.7.1
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-angular-templates');
+  //grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
-
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
-
-  // Define the configuration for all the tasks
   grunt.initConfig({
-
-    // Project settings
-    yeoman: {
-      // configurable paths
-      app: require('./bower.json').appPath || 'app',
-      dist: '../resources/public'
-    },
-
-    // Watches files for changes and runs tasks based on the changed files
-    watch: {
-      coffee: {
-        files: ['<%= yeoman.app %>/scripts/**/*.{coffee,litcoffee,coffee.md}'],
-        tasks: ['newer:coffee:dist']
-      },
-      coffeeTest: {
-        files: ['test/spec/**/*.{coffee,litcoffee,coffee.md}'],
-        tasks: ['newer:coffee:test', 'karma']
-      },
-      less: {
-        files: ['<%= yeoman.app %>/less/**/*.less'],
-        tasks: ['newer:copy:styles', 'autoprefixer']
-      },
-      gruntfile: {
-        files: ['Gruntfile.js']
-      },
-      // livereload: {
-      //   options: {
-      //     livereload: '<%= connect.options.livereload %>'
-      //   },
-      //   files: [
-      //     '<%= yeoman.app %>/**/*.html',
-      //     '.tmp/styles/**/*.css',
-      //     '.tmp/scripts/**/*.js',
-      //     '<%= yeoman.app %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}'
-      //   ]
-      // },
-      reload: {
-        files: [
-          '<%= yeoman.app %>/**/*.html',
-          '.tmp/styles/**/*.css',
-          '.tmp/scripts/**/*.js',
-          '<%= yeoman.app %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}'
-        ],
-        tasks: ['build']
-      }
-    },
-
-    // The actual grunt server settings
-    connect: {
-      options: {
-        port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
-      },
-      livereload: {
-        options: {
-          open: true,
-          base: [
-            '.tmp',
-            '<%= yeoman.app %>'
-          ]
-        }
-      },
-      test: {
-        options: {
-          port: 9001,
-          base: [
-            '.tmp',
-            'test',
-            '<%= yeoman.app %>'
-          ]
-        }
-      },
-      dist: {
-        options: {
-          base: '<%= yeoman.dist %>'
-        }
-      }
-    },
-
-    // Make sure code styles are up to par and there are no obvious mistakes
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      all: [
-        'Gruntfile.js'
-      ]
-    },
-
-    // Empties folders to start fresh
     clean: {
-      dist: {
-        options: { force: true },
-        files: [{
-          dot: true,
-          src: [
-            '.tmp',
-            '<%= yeoman.dist %>/*',
-            '!<%= yeoman.dist %>/.git*'
-          ]
-        }]
-      },
-      server: '.tmp'
+      options: { force: true },
+      main: ['../resources/public/assets/**/*']
     },
-
-    // Add vendor prefixed styles
-    autoprefixer: {
-      options: {
-        browsers: ['last 1 version']
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/styles/',
-          src: '**/*.css',
-          dest: '.tmp/styles/'
-        }]
-      }
-    },
-
-    // Automatically inject Bower components into the app
-    'bower-install': {
-      app: {
-        html: '<%= yeoman.app %>/index.html',
-        ignorePath: '<%= yeoman.app %>/'
-      }
-    },
-
-
-    // Compiles CoffeeScript to JavaScript
     coffee: {
-      options: {
-        sourceMap: true,
-        sourceRoot: ''
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/scripts',
-          src: '**/*.coffee',
-          dest: '.tmp/scripts',
-          ext: '.js'
-        }]
-      },
-      test: {
-        files: [{
-          expand: true,
-          cwd: 'test/spec',
-          src: '**/*.coffee',
-          dest: '.tmp/spec',
-          ext: '.js'
-        }]
+      app: {
+        options: { join: true },
+        files: { '../resources/public/assets/js/main.js': 'coffee/**/*.coffee' }
       }
     },
-
-
-
+   ngtemplates: {
+      app: {
+        src: 'views/**/*.html',
+        dest: '../resources/public/assets/js/views.js',
+        options: {
+          module: 'fhirplaceGui',
+          prefix: '/'
+        }
+      }
+    },
     less: {
       dist: {
         files: [{
           expand: true,
-          cwd: '<%= yeoman.app %>/less',
-          src: '**/*.less',
-          dest: '.tmp/styles',
+          cwd: 'less',
+          src: ['*.less', '!.*#.less'],
+          dest: '../resources/public/assets/css/',
           ext: '.css'
         }]
       }
     },
-
-
-    // Renames files for browser caching purposes
-    rev: {
-      dist: {
-        files: {
-          src: [
-            '<%= yeoman.dist %>/scripts/**/*.js',
-            '<%= yeoman.dist %>/styles/**/*.css',
-            '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*'
-          ]
+    concat: {
+      lib_js: {
+        src: [
+          "lib/jquery/dist/jquery.min.js",
+          "lib/angular/angular.js",
+          "lib/angular-formstamp/build/formstamp.js",
+          "lib/angular-route/angular-route.js",
+          "lib/angular-animate/angular-animate.js",
+          "lib/angular-cookies/angular-cookies.js",
+          "lib/angular-sanitize/angular-sanitize.js",
+          "lib/codemirror/lib/codemirror.js",
+          "lib/codemirror/mode/sql/sql.js",
+          "lib/codemirror/mode/javascript/javascript.js",
+          "lib/angular-ui-codemirror/ui-codemirror.js"
+        ],
+        dest: '../resources/public/assets/js/lib.js'
+      },
+      app_js: {
+        src: [ '../resources/public/assets/js/main.js',
+        '../resources/public/assets/js/views.js' ],
+        dest: '../resources/public/assets/js/app.js'
+      },
+      lib_css: {
+        src: ['lib/components-font-awesome/css/font-awesome.min.css',
+        'lib/codemirror/lib/codemirror.css',
+        'lib/bootstrap/dist/css/bootstrap.min.css',
+        "lib/angular-formstamp/build/formstamp.css",
+        ],
+        dest: '../resources/public/assets/css/lib.css'
+      }
+    },
+    copy: {
+      bs_fonts: {
+        cwd: 'lib/bootstrap/dist/fonts/',
+        expand: true,
+        src: '*',
+        dest: '../resources/public/assets/fonts/'
+      },
+      fa_fonts: {
+        cwd: 'lib/components-font-awesome/fonts/',
+        expand: true,
+        src: '*',
+        dest: '../resources/public/assets/fonts/'
+      },
+      hs_fonts: {
+        cwd: 'fonts/',
+        expand: true,
+        src: '*',
+        dest: '../resources/public/assets/fonts/'
+     },
+     index: {
+       src: 'index.html',
+       dest: '../resources/public/index.html'
+     }
+    },
+    watch: {
+      main: {
+        files: ['views/**/*', 'index.html','coffee/**/*.coffee', 'less/**/*.less'],
+        tasks: ['build'],
+        options: {
+          events: ['changed', 'added'],
+          nospawn: true
         }
       }
-    },
-
-    // Reads HTML for usemin blocks to enable smart builds that automatically
-    // concat, minify and revision files. Creates configurations in memory so
-    // additional tasks can operate on them
-    useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
-      options: {
-        dest: '<%= yeoman.dist %>'
-      }
-    },
-
-    // Performs rewrites based on rev and the useminPrepare configuration
-    usemin: {
-      html: ['<%= yeoman.dist %>/**/*.html'],
-      css: ['<%= yeoman.dist %>/styles/**/*.css'],
-      options: {
-        assetsDirs: ['<%= yeoman.dist %>']
-      }
-    },
-
-    // The following *-min tasks produce minified files in the dist folder
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '**/*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '**/*.svg',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-    htmlmin: {
-      dist: {
-        options: {
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeCommentsFromCDATA: true,
-          removeOptionalTags: true
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/**/*.html'],
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
-
-    // Allow the use of non-minsafe AngularJS files. Automatically makes it
-    // minsafe compatible so Uglify does not destroy the ng references
-    ngmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/concat/scripts',
-          src: '*.js',
-          dest: '.tmp/concat/scripts'
-        }]
-      }
-    },
-
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
-    // Copies remaining files to places other tasks can use
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '<%= yeoman.dist %>',
-          src: [
-            '*.{ico,png,txt}',
-            '.htaccess',
-            '*.html',
-            'views/**/*.html',
-            'bower_components/**/*',
-            'images/**/*.{webp}',
-            'fonts/*',
-            '.nojekyll'
-          ]
-        }, {
-          expand: true,
-          cwd: '.tmp/images',
-          dest: '<%= yeoman.dist %>/images',
-          src: ['generated/*']
-        }]
-      },
-      styles: {
-        expand: true,
-        cwd: '.tmp/styles',
-        dest: '<%= yeoman.dist %>/styles',
-        src: '**/*.css'
-      },
-      js: {
-        expand: true,
-        cwd: '.tmp/scripts',
-        dest: '<%= yeoman.dist %>/scripts',
-        src: '**/*.js'
-      }
-    },
-
-    // Run some tasks in parallel to speed up the build process
-    concurrent: {
-      server: [
-        'coffee:dist',
-        'less:dist'
-      ],
-      test: [
-        'coffee',
-        'less'
-      ],
-      dist: [
-        'coffee',
-        'less',
-        'imagemin',
-        'svgmin'
-      ]
-    },
-
-    concat: {
-      dist: {}
-    },
-
-    // By default, your `index.html`'s <!-- Usemin block --> will take care of
-    // minification. These next options are pre-configured if you do not wish
-    // to use the Usemin blocks.
-    // cssmin: {
-    //   dist: {
-    //     files: {
-    //       '<%= yeoman.dist %>/styles/main.css': [
-    //         '.tmp/styles/**/*.css',
-    //         '<%= yeoman.app %>/styles/**/*.css'
-    //       ]
-    //     }
-    //   }
-    // },
-    // uglify: {
-    //   dist: {
-    //     files: {
-    //       '<%= yeoman.dist %>/scripts/scripts.js': [
-    //         '<%= yeoman.dist %>/scripts/scripts.js'
-    //       ]
-    //     }
-    //   }
-    // },
-    // concat: {
-    //   dist: {}
-    // },
-
-    // Test settings
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
-      }
     }
   });
 
-
-  grunt.registerTask('serve', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
-    }
-
-    grunt.task.run([
-      'clean:server',
-      'bower-install',
-      'concurrent:server',
-      'autoprefixer',
-      'connect:livereload',
-      'watch'
-    ]);
-  });
-
-  grunt.registerTask('server', function () {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve']);
-  });
-
-  grunt.registerTask('test', [
-    'clean:server',
-    'concurrent:test',
-    'autoprefixer',
-    'connect:test',
-    'karma'
-  ]);
-
-  grunt.registerTask('build', [
-    'clean:dist',
-    'bower-install',
-    'concurrent:dist',
-    'concat',
-    'copy:dist',
-    'copy:js',
-    'autoprefixer',
-    'copy:styles',
-    'cdnify'
-  ]);
-
-  grunt.registerTask('deploy', [
-    'clean:dist',
-    'bower-install',
-    'useminPrepare',
-    'concurrent:dist',
-    'concat',
-    'ngmin',
-    'copy:dist',
-    'copy:js',
-    'autoprefixer',
-    'copy:styles',
-    'cdnify',
-    'cssmin',
-    'uglify',
-    'rev',
-    'usemin',
-    'htmlmin'
-  ]);
-
-  grunt.registerTask('default', [
-    'newer:jshint',
-    'test',
-    'build'
-  ]);
+  grunt.registerTask('build', ['clean', 'coffee', 'less', 'ngtemplates', 'concat', 'copy']);
 };
---
+++ gui/coffee/app.coffee
@@ -0,0 +1,150 @@
+'use strict'
+
+app = angular.module 'fhirplaceGui', [
+  'ngCookies',
+  'ngAnimate',
+  'ngSanitize',
+  'ngRoute',
+  'formstamp',
+  "ui.codemirror"
+], ($routeProvider) ->
+    console.log('Im here')
+
+    $routeProvider
+      .when '/',
+        templateUrl: '/views/welcome.html'
+      .when '/conformance',
+        templateUrl: '/views/conformance.html'
+        controller: 'ConformanceCtrl'
+      .when '/resources/:resourceType',
+        templateUrl: '/views/resources/index.html'
+        controller: 'ResourcesIndexCtrl'
+      .when '/resources/:resourceType/new',
+        templateUrl: '/views/resources/new.html'
+        controller: 'ResourcesNewCtrl'
+      .when '/resources/:resourceType/:resourceLogicalId',
+        templateUrl: '/views/resources/show.html'
+        controller: 'ResourceCtrl'
+      .when '/resources/:resourceType/:resourceLogicalId/edit',
+        templateUrl: '/views/resources/edit.html'
+        controller: 'ResourcesShowCtrl'
+      .when '/resources/:resourceType/:resourceLogicalId/validate',
+        templateUrl: '/views/resources/validate.html'
+        controller: 'ResourcesShowCtrl'
+      .when '/resources/:resourceType/:resourceLogicalId/history',
+        templateUrl: '/views/resources/history.html'
+        controller: 'ResourcesHistoryCtrl'
+      .otherwise
+        redirectTo: '/'
+
+app.filter 'uuid', ()->
+  (id)->
+    sid = id.substring(id.length - 6, id.length)
+    "...#{sid}"
+
+app.controller 'ConformanceCtrl', ($scope, $http) ->
+    $scope.restRequestMethod = 'GET'
+    $scope.restUri = '/metadata?_format=application/json'
+
+    $http.get($scope.restUri)
+      .success (data, status, headers, config) ->
+        $scope.resources = data.rest[0].resources.sort (a, b) ->
+          return -1 if a.type < b.type
+          return 1 if a.type > b.type
+          0
+        data.rest = null
+        $scope.conformance = data
+        $scope.serverName = data.name
+        $scope.resourceTypes = data.rest[0].resources
+      .error (data, status, headers, config) ->
+        console.log 'karamba'
+
+
+app.controller 'ResourcesIndexCtrl', ($scope, $routeParams, $http) ->
+  $scope.resourceType      = $routeParams.resourceType
+  $scope.resourceTypeLabel = $scope.resourceType
+  $scope.restRequestMethod = 'GET'
+  $scope.restUri =
+    "/#{$scope.resourceType}/_search?_format=application/json"
+
+  $http.get($scope.restUri).success (data, status, headers, config) ->
+    $scope.resources = data.map (resource) ->
+      resource.prettyData = angular.toJson(angular.fromJson(resource.data), true)
+      resource
+
+app.controller 'ResourcesNewCtrl', ($scope, $routeParams, $http) ->
+  $scope.resourceType      = $routeParams.resourceType
+  $scope.resourceTypeLabel = $routeParams.resourceType
+  $scope.restRequestMethod = 'POST'
+  $scope.restUri = "/#{$scope.resourceType}?_format=application/json"
+  $scope.resource = {}
+
+  headers = {'Content-Location': $scope.resourceContentLocation}
+  $scope.save = ->
+    $http(method: $scope.restRequestMethod, url: $scope.restUri, data: $scope.resource.content, headers: headers)
+      .success (data, status, headers, config) ->
+        $location.path("/resources/#{$scope.resourceType}")
+
+app.controller 'ResourceCtrl', ($scope, $routeParams, $http) ->
+  $scope.resourceType       = $routeParams.resourceType
+  $scope.resourceLogicalId = $routeParams.resourceLogicalId
+  $scope.resourceLabel     = $scope.resourceLogicalId
+  $scope.restRequestMethod = 'GET'
+  $scope.restUri =
+    "/#{$scope.resourceType}/#{$scope.resourceLogicalId}?_format=application/json"
+
+  loadResource = ()->
+    $http.get($scope.restUri).success (data, status, headers, config) ->
+      $scope.resource = {
+        content: angular.toJson(angular.fromJson(data), true)
+      }
+      $scope.resourceContentLocation = headers('content-location')
+
+  loadResource()
+
+  $scope.save = ->
+    $http(method: "PUT", url: $scope.restUri, data: $scope.resource.content, headers: {'Content-Location': $scope.resourceContentLocation})
+      .success (data, status, headers, config) ->
+        loadResource()
+      .error (data)->
+        $scope.error = data
+
+  $scope.destroy = ->
+    if window.confirm("Destroy #{$scope.resourceTypeLabel} #{$scope.resourceLabel}?")
+      $http.delete($scope.restUri).success (data, status, headers, config) ->
+        $location.path("/resources/#{$scope.resourceType}")
+
+app.controller 'ResourcesHistoryCtrl', ($scope, $routeParams, $http) ->
+  angular.extend($scope, $routeParams)
+  $scope.restRequestMethod = 'GET'
+  $scope.restUri =
+    "/#{$scope.resourceType}/#{$scope.resourceLogicalId}/_history?_format=application/json"
+
+  $http.get($scope.restUri).success (data, status, headers, config) ->
+    $scope.resourceHistory  = angular.toJson(angular.fromJson(data), true)
+    $scope.resourceVersions = data.entry
+
+
+angular.module('fhirplaceGui')
+  .controller 'ResourcesValidateCtrl', ($scope, $http) ->
+    $scope.restRequestMethod = 'POST'
+    $scope.restUri =
+      "/#{$scope.resourceType}/_validate?_format=application/json"
+
+    $scope.validate = ->
+      if $scope.form.$valid
+        $scope.resourceValidation = 'Validating ...'
+        $http.post($scope.restUri, $scope.resource.prettyData)
+          .success((data, status, headers, config) ->
+            if data
+              $scope.resourceValidation = angular.toJson(
+                angular.fromJson(data),
+                true
+              )
+            else
+              $scope.resourceValidation = 'Everything is good'
+          ).error (data, status, headers, config) ->
+            $scope.resourceValidation = angular.toJson(
+              angular.fromJson(data),
+              true
+            )
